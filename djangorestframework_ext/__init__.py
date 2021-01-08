804093ebc1ee4aed03dcc67a7bb37d5bb77bb686
zengqiu <zengqiu@qq.com>
2021-01-08 13:51:55 +0800

    Fixed something

--- djangorestframework_ext/__init__.py
+++ djangorestframework_ext/__init__.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-
 from __future__ import absolute_import
 
-from .widgets import *
-from .fields import *
-from .validators import *
-from .utils import *
 
 name = 'djangorestframework-ext'
 __version__ = '0.1'
