b4cbd2a9361149bcd5e4df2b1c31ffb0b868d53c
zengqiu <zengqiu@qq.com>
2022-10-19 12:44:41 +0800

    Fix import error

--- rest_framework_ext/pagination.py
+++ rest_framework_ext/pagination.py
@@ -1,10 +1,10 @@
 from rest_framework.pagination import PageNumberPagination
-from .settings import rest_framework_ext_settings
+from .settings import api_settings
 
 
 class DynamicSizePageNumberPagination(PageNumberPagination):
-    page_query_param = rest_framework_ext_settings.PAGE_QUERY_PARAM
-    page_size_query_param = rest_framework_ext_settings.PAGE_SIZE_QUERY_PARAM
+    page_query_param = api_settings.PAGE_QUERY_PARAM
+    page_size_query_param = api_settings.PAGE_SIZE_QUERY_PARAM
 
     def paginate_queryset(self, queryset, request, view=None):
         if self.page_query_param not in request.query_params:
