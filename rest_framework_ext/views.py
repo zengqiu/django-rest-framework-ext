6e94c94381ea9dbf11b9a04b949e4c76be928713
zengqiu <zengqiu@qq.com>
2023-05-29 11:27:59 +0800

    Fix unclear error messages

--- rest_framework_ext/views.py
+++ rest_framework_ext/views.py
@@ -8,9 +8,7 @@ from rest_framework import status
 def exception_handler(exc, context):
     response = exc_handler(exc, context)
 
-    if isinstance(exc, IntegrityError) and not response:
-        response = Response('唯一约束', status=status.HTTP_400_BAD_REQUEST)
-    elif isinstance(exc, ValidationError) and not response:
-        response = Response(exc, status=status.HTTP_400_BAD_REQUEST)
+    if isinstance(exc, (IntegrityError, ValidationError)) and not response:
+        response = Response(str(exc), status=status.HTTP_400_BAD_REQUEST)
 
     return response
