1a7666cacafa3635c23f094f93c378fd8aa527e0
zengqiu <zengqiu@qq.com>
2024-01-26 09:26:59 +0800

    User DRF default page_query_param

--- rest_framework_ext/settings.py
+++ rest_framework_ext/settings.py
@@ -1,9 +1,10 @@
 from django.conf import settings
+from rest_framework.settings import api_settings
 
 USER_SETTINGS = getattr(settings, 'REST_FRAMEWORK_EXT', {})
 
 DEFAULTS = {
-    'PAGE_QUERY_PARAM': 'page',
+    'PAGE_QUERY_PARAM': api_settings.DEFAULT_PAGINATION_CLASS.page_query_param,
     'PAGE_SIZE_QUERY_PARAM': 'limit',
 }
 
