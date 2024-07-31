055558d88dc24b814971d0663de27fdf86f217ac
zengqiu <zengqiu@qq.com>
2024-07-31 11:26:35 +0800

    Remove useless import

--- rest_framework_ext/viewsets.py
+++ rest_framework_ext/viewsets.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from .permissions import ExportPermission
